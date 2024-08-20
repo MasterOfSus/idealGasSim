3f9dfb2aab7eb832ae06c000e3aeba0ca015023a
CetrioloRiick <diego.q@ik.me>
2024-08-20 09:52:28 +0200

    moved all member functions into cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,32 @@
 
 #include "namespacer.hpp"
 
+namespace thermo {
+// all class member functions
+
+double PhysVector::get_module() { return std::sqrt(x * x + y * y + z * z); }
+PhysVector::PhysVector(double Ix, double Iy, double Iz) : x{Ix}, y{Iy}, z{Iz} {}
+
+Particle::Particle(PhysVector Ipos, PhysVector Ispeed)
+      : position_{Ipos}, speed_{Ispeed} {}
+
+
+thermo::Gas::Gas(int n) {
+  std::default_random_engine eng(std::time(nullptr));
+  std::uniform_real_distribution<double> dist(0.0, 1.0);
+  for (int i{0}; i != n; ++i) {
+    particles_.push_back(
+        {{dist(eng), dist(eng), dist(eng)}, {dist(eng), dist(eng), dist(eng)}});
+  }
+}
+// end of member functions
+
+}  // namespace thermo
+
 int main() {
   // QUI INTERFACCIA UTENTE INSERIMENTO DATI
-  thermo::Gas gas{10}; //Creazione del gas con particelle randomizzate
+  thermo::PhysVector prova;
+  std::cout << prova.get_module();
+  thermo::Gas gas{10};  // Creazione del gas con particelle randomizzate
+
 }
--- namespacer.hpp
+++ namespacer.hpp
@@ -7,9 +7,8 @@
 namespace thermo {
 struct PhysVector {
   double x, y, z;
-  double get_module() { return std::sqrt(x * x + y * y + z * z); }
-  PhysVector(double Ix = 0.0, double Iy = 0.0, double Iz = 0.0)
-      : x{Ix}, y{Iy}, z{Iz} {}
+  double get_module();
+  PhysVector(double Ix = 0.0, double Iy = 0.0, double Iz = 0.0);
 };
 
 class Particle {
@@ -20,21 +19,13 @@ class Particle {
   PhysVector momentum_;
 
  public:
-  Particle(PhysVector Ipos, PhysVector Ispeed)
-      : position_{Ipos}, speed_{Ispeed} {}
+  Particle(PhysVector Ipos, PhysVector Ispeed);
 };
 class Gas {
   std::vector<Particle> particles_{};
   void update_gas_state();  // called in each iteration of the game loop
  public:
-  Gas(int n) {
-    std::default_random_engine eng(std::time(nullptr));
-    std::uniform_real_distribution<double> dist(0.0, 1.0);
-    for (int i{0}; i != n; ++i) {
-      particles_.push_back({{dist(eng), dist(eng), dist(eng)},
-                            {dist(eng), dist(eng), dist(eng)}});
-    }
-  }
+  Gas(int n);
   double get_pressure();     // returns the value of the pressure of the gas
   int get_volume();          // returns the volume of the gas
   double get_temperature();  // returns the temperature of the gas
