48999f598c9fddfd616532773d2ded781ada40d6
CetrioloRiick <diego.q@ik.me>
2024-08-21 19:25:58 +0200

    first implementation of the banging balls

--- main.cpp
+++ main.cpp
@@ -16,15 +16,73 @@ Particle::Particle(PhysVector Ipos, PhysVector Ispeed)
     : position_{Ipos}, speed_{Ispeed} {}
 PhysVector Particle::get_speed() { return speed_; };
 PhysVector Particle::get_position() { return position_; };
-PhysVector Particle::get_momentum() { return (speed_ * mass_); };
+// PhysVector Particle::get_momentum() { return (speed_ * mass_); };
 
-thermo::Gas::Gas(int n) {
+Gas::Gas(int n) {
   std::default_random_engine eng(std::time(nullptr));
-  std::uniform_real_distribution<double> dist(0.0, 1.0);
+  std::uniform_real_distribution<double> dist(0.0, 20.0);
   for (int i{0}; i != n; ++i) {
+    std::cout << "costruita " << i << '\n';
     particles_.push_back(
         {{dist(eng), dist(eng), dist(eng)}, {dist(eng), dist(eng), dist(eng)}});
   }
+}
+
+double Gas::time_impact(std::vector<Particle>::iterator P1,
+                        std::vector<Particle>::iterator P2) {
+  double a{0};
+  double b{0};
+  double c{0};
+  double result{10000};
+
+  a = std::pow(((*P1).get_speed().x - (*P2).get_speed().x), 2);
+  a = a + std::pow(((*P1).get_speed().y - (*P2).get_speed().y), 2);
+  a = a + std::pow(((*P1).get_speed().z - (*P2).get_speed().z), 2);
+  std::cout << a << '\n';
+
+  b = ((*P1).get_position().x - (*P2).get_position().x) *
+      ((*P1).get_speed().x - (*P2).get_speed().x);
+  b = b + ((*P1).get_position().y - (*P2).get_position().y) *
+              ((*P1).get_speed().y - (*P2).get_speed().y);
+  b = b + ((*P1).get_position().z - (*P2).get_position().z) *
+              ((*P1).get_speed().z - (*P2).get_speed().z);
+  std::cout << b << '\n';
+
+  c = std::pow(((*P1).get_position().x - (*P2).get_position().x), 2);
+  c = c + std::pow(((*P1).get_position().y - (*P2).get_position().y), 2);
+  c = c + std::pow(((*P1).get_position().z - (*P2).get_position().z), 2) -
+      4 * pow((*P1).radius, 2);
+  std::cout << c << '\n';
+
+  double delta{std::pow(b, 2) - a * c};
+
+  if (delta>0){
+    delta=sqrt(delta);
+    double t1{(-b-delta)/a};
+    double t2{(-b+delta)/a};
+    if (t1>0){
+      result=t1;
+    }else if(t2>0){
+      result=t2;
+    }
+  }
+
+  return result;
+}
+
+double Gas::find_iteration() {
+  double shortest{1000};
+  for (auto it = particles_.begin(), last = particles_.end(); it != last;
+       ++it) {
+    for (auto it2 = particles_.begin(), last2 = particles_.end(); it2 != last2;
+         ++it2) {
+      double time{time_impact(it, it2)};
+      if (time < shortest) {
+        shortest = time;
+      }
+    }
+  }
+  return shortest;
 }
 // end of member functions
 
@@ -35,4 +93,5 @@ int main() {
   thermo::PhysVector prova;
   std::cout << prova.get_module();
   thermo::Gas gas{10};  // Creazione del gas con particelle randomizzate
+  gas.find_iteration();
 }
--- namespacer.hpp
+++ namespacer.hpp
@@ -1,6 +1,5 @@
 #include <cmath>
 #include <ctime>
-#include <iostream>
 #include <vector>
 
 namespace thermo {
@@ -12,27 +11,32 @@ struct PhysVector {
 };
 
 class Particle {
-  static double mass_;
-  short int last_collision_;
+  //static double mass_;
+  
+  //int last_collision_;
   PhysVector position_;
   PhysVector speed_;
-  PhysVector momentum_;
+  //PhysVector momentum_;
 
  public:
+  double radius=5.;
   PhysVector get_speed();
   PhysVector get_position();
-  PhysVector get_momentum();
+  //PhysVector get_momentum();
 
- public:
   Particle(PhysVector Ipos, PhysVector Ispeed);
 };
 class Gas {
   std::vector<Particle> particles_{};
   void update_gas_state();  // called in each iteration of the game loop
+  double time_impact(std::vector<Particle>::iterator a,
+                     std::vector<Particle>::iterator b);
+
  public:
   Gas(int n);
-  double get_pressure();     // returns the value of the pressure of the gas
-  int get_volume();          // returns the volume of the gas
-  double get_temperature();  // returns the temperature of the gas
+  double find_iteration();
+  //double get_pressure();     // returns the value of the pressure of the gas
+  //double get_volume();       // returns the volume of the gas
+  //double get_temperature();  // returns the temperature of the gas
 };
 }  // namespace thermo
