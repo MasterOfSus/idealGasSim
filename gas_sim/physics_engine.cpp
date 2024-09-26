85bf0b2eec029530f5755deb211b3da12efeff9e
CetrioloRiick <diego.q@ik.me>
2024-09-26 22:04:31 +0200

    Re-download .clang-format

--- gas_sim/physics_engine.cpp
+++ gas_sim/physics_engine.cpp
@@ -1,45 +1,34 @@
 #include "physics_engine.hpp"
+
 #include <cmath>
 
 namespace gasSim {
 namespace physics {
 
-vector vector::operator+(const vector& v)
-{
+vector vector::operator+(const vector& v) {
   return {x + v.x, y + v.y, z + v.z};
 }
-vector vector::operator-(const vector& v)
-{
+vector vector::operator-(const vector& v) {
   return {x - v.x, y - v.y, z - v.z};
 }
 
-vector vector::operator*(double scalar)
-{
+vector vector::operator*(double scalar) {
   return {x * scalar, y * scalar, z * scalar};
 }
-vector vector::operator/(double scalar)
-{
+vector vector::operator/(double scalar) {
   return {x / scalar, y / scalar, z / scalar};
 }
 
-double vector::operator*(const vector& v)
-{
+double vector::operator*(const vector& v) {
   return x * v.x + y * v.y + z * v.z;
 }
 
-bool vector::operator==(const vector& v) const
-{
+bool vector::operator==(const vector& v) const {
   return (x == v.x && y == v.y && z == v.z);
 }
-bool vector::operator!=(const vector& v) const
-{
-  return !(*this == v);
-}
+bool vector::operator!=(const vector& v) const { return !(*this == v); }
 
-double vector::norm()
-{
-  return std::sqrt(x * x + y * y + z * z);
-}
+double vector::norm() { return std::sqrt(x * x + y * y + z * z); }
 
-} // namespace physics
-} // namespace thermo
+}  // namespace physics
+}  // namespace gasSim
