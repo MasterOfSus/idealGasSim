8ae87ad8f6b555bdda1a2fb51f5e4d6fb6ddaafd
CetrioloRiick <diego.q@ik.me>
2024-09-26 21:37:52 +0200

    Added unit test for physics.vector

--- unit_testing/unit_tests.cpp
+++ unit_testing/unit_tests.cpp
@@ -1,8 +1,51 @@
+#include <cmath>
+#include <iostream>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include "doctest.h"
 
-#include "../gas_sim2/graphics.hpp"
+#include "../gas_sim/graphics.hpp"
 // #include "input.hpp"
-#include "../gas_sim2/physics_engine.hpp"
-#include "../gas_sim2/output.hpp"
+#include "../gas_sim/output.hpp"
+#include "../gas_sim/physics_engine.hpp"
+
+TEST_CASE("Testing physics.vector")
+{
+  thermo::physics::vector v1{1, 2, 3};
+  thermo::physics::vector v2{0, 0, -1};
+
+  SUBCASE("Sum")
+  {
+    thermo::physics::vector sum{v1 + v2};
+    thermo::physics::vector test{1, 2, 2};
+    CHECK(sum == test);
+  }
+  SUBCASE("Subtraction")
+  {
+    thermo::physics::vector sub{v2 - v1};
+    thermo::physics::vector test{-1, -2, -4};
+    CHECK(sub == test);
+  }
+  SUBCASE("Product")
+  {
+    double scalar{0.25};
+    thermo::physics::vector test{0.25, 0.5, 0.75};
+    CHECK(v1 * scalar == test);
+  }
+  SUBCASE("Division")
+  {
+    double scalar{10};
+    thermo::physics::vector test{0.1, 0.2, 0.3};
+    CHECK(v1 / scalar == test);
+  }
+  SUBCASE("Division by zero")
+  {
+    double scalar{0};
+    thermo::physics::vector test{INFINITY, INFINITY, INFINITY};
+    CHECK(v1 / scalar == test);
+  }
+  SUBCASE("Norm")
+  {
+    CHECK(v1.norm() == doctest::Approx(3.74166));
+  }
+}
