a994196a46a03ddf89115b9cf6e539d776d1f733
CetrioloRiick <diego.q@ik.me>
2025-03-26 00:47:44 +0100

    "working" graphicsTest

--- graphicsTest.cpp
+++ graphicsTest.cpp
@@ -13,22 +13,21 @@
 #include "gasSim/physicsEngine.hpp"
 #include "gasSim/statistics.hpp"
 
-double gasSim::Particle::radius = 1;
+double gasSim::Particle::radius = 0.1;
 double gasSim::Particle::mass = 10;
 
-
 int main() {
-  gasSim::Gas amogus(2, 1., 10.);
+  gasSim::Gas amogus(200, 1., 10.);
   gasSim::PhysVectorF focus{23., 24., 26.};
   gasSim::PhysVectorF center{5., 5., 5.};
   gasSim::Camera camera(focus, center - focus, 2., 90., 1200, 900);
 
   sf::RenderTexture photo;
   photo.create(1200, 900);
-	sf::CircleShape shape {1.f};
-	shape.setFillColor(sf::Color::Red);
-	shape.setOutlineColor(sf::Color::Black);
-	shape.setOutlineThickness(10.);
+  sf::CircleShape shape{1.f};
+  shape.setFillColor(sf::Color::Red);
+  shape.setOutlineColor(sf::Color::Black);
+  shape.setOutlineThickness(10.);
   gasSim::RenderStyle style{shape};
   std::string options{"udlrfb"};
   style.setWallsOpts(options);
@@ -38,39 +37,41 @@ int main() {
   sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window",
 
                           sf::Style::Default);
-	
-	amogus.simulate(8);
 
-	gasSim::drawGas(amogus, camera, photo, style);
+  amogus.simulate(8);
 
+  gasSim::drawGas(amogus, camera, photo, style);
+  gasSim::TdStats stats(amogus);
   // run the program as long as the window is open
   while (window.isOpen()) {
     // check all the window's events that were triggered since the last
     // iteration of the loop
     sf::Event event;
 
-    sleep(1./30.);
+   
+    //sf::sleep(sf::milliseconds(90));
     amogus.simulate(1);
-		gasSim::drawGas(amogus, camera, photo, style);
+    gasSim::drawGas(amogus, camera, photo, style);
 
-		std::vector<gasSim::Particle> particles = amogus.getParticles();
+    std::vector<gasSim::Particle> particles = amogus.getParticles();
 
-		std::cout << "Particles poss and speeds:\n";
+   // std::cout << "Particles poss and speeds:\n";
 
-		for (const gasSim::Particle& particle: particles) {
-			gasSim::PhysVectorD vector = particle.position;
-			std::cout << "(" << vector.x << ", " << vector.y << ", " << vector.z << "), (";
-			vector = particle.speed;
-			std::cout << vector.x << ", " << vector.y << ", " << vector.z << ")\n";
-		}
+    for (const gasSim::Particle& particle : particles) {
+      gasSim::PhysVectorD vector = particle.position;
+     // std::cout << "(" << vector.x << ", " << vector.y << ", " << vector.z
+             //   << "), (";
+      vector = particle.speed;
+     // std::cout << vector.x << ", " << vector.y << ", " << vector.z << ")\n";
+    }
 
     while (window.pollEvent(event)) {
       // "close requested" event: we close the window
       if (event.type == sf::Event::Closed) window.close();
     }
-		window.clear(sf::Color::Yellow);
-		window.draw(picture);
-		window.display();
+    window.clear(sf::Color::Yellow);
+    window.draw(picture);
+    window.display();
   }
   gasSim::printInitData(1, 2, 3, 4, false);
   gasSim::printStat(stats);
