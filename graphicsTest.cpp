62f885720d979308846f435dae7270ad6441bd97
MasterOfSus <cinnamontophat@gmail.com>
2025-03-26 00:20:44 +0100

    Buggyyyyy

--- graphicsTest.cpp
+++ graphicsTest.cpp
@@ -17,13 +17,17 @@ double gasSim::Particle::mass = 10;
 
 int main() {
   gasSim::Gas amogus(2, 1., 10.);
-  gasSim::PhysVectorF focus{-23., 31., 12.};
-  gasSim::PhysVectorF center{4.5, 4.5, 4.5};
+  gasSim::PhysVectorF focus{23., 24., 26.};
+  gasSim::PhysVectorF center{5., 5., 5.};
   gasSim::Camera camera(focus, center - focus, 2., 90., 1200, 900);
 
   sf::RenderTexture photo;
   photo.create(1200, 900);
-  gasSim::RenderStyle style{};
+	sf::CircleShape shape {1.f};
+	shape.setFillColor(sf::Color::Red);
+	shape.setOutlineColor(sf::Color::Black);
+	shape.setOutlineThickness(10.);
+  gasSim::RenderStyle style{shape};
   std::string options{"udlrfb"};
   style.setWallsOpts(options);
   sf::Sprite picture;
@@ -31,24 +35,39 @@ int main() {
 
   sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window",
                           sf::Style::Default);
+	
+	amogus.simulate(8);
+
+	gasSim::drawGas(amogus, camera, photo, style);
 
   // run the program as long as the window is open
   while (window.isOpen()) {
     // check all the window's events that were triggered since the last
     // iteration of the loop
     sf::Event event;
-    gasSim::drawGas(amogus, camera, photo, style);
 
-    sleep(1);
+    sleep(1./30.);
     amogus.simulate(1);
+		gasSim::drawGas(amogus, camera, photo, style);
+
+		std::vector<gasSim::Particle> particles = amogus.getParticles();
+
+		std::cout << "Particles poss and speeds:\n";
+
+		for (const gasSim::Particle& particle: particles) {
+			gasSim::PhysVectorD vector = particle.position;
+			std::cout << "(" << vector.x << ", " << vector.y << ", " << vector.z << "), (";
+			vector = particle.speed;
+			std::cout << vector.x << ", " << vector.y << ", " << vector.z << ")\n";
+		}
 
     while (window.pollEvent(event)) {
       // "close requested" event: we close the window
       if (event.type == sf::Event::Closed) window.close();
     }
-    window.clear(sf::Color::White);
-    window.draw(picture);
-    window.display();
+		window.clear(sf::Color::Yellow);
+		window.draw(picture);
+		window.display();
   }
 
   return 0;
